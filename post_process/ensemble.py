3d3e55b53478781b1a4b75519625594ac33d8c86
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2025-01-22 13:37:40 +0100

    Fixed target not saved in ensemble files

--- post_process/ensemble.py
+++ post_process/ensemble.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
             for pid in targets.keys():
                 pred = np.mean(ensemble_prediction[pid], axis=-1)
                 f['predicted'].create_dataset(pid, data=pred)
-                f['y'].create_dataset(pid, data=targets)
+                f['y'].create_dataset(pid, data=targets[pid])
                 ensemble_info.append([pid, f1_score(targets[pid], pred), precision(targets[pid], pred), recall(targets[pid], pred)])
 
     df = pd.DataFrame(ensemble_info, columns=['pid', 'f1', 'precision', 'recall'])

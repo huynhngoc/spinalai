97483d0622c3aa31b3c44e787d8c0a7f6cb48760
Esther Zijerveld <esther.maud.zijerveld@nmbu.no>
2025-02-04 14:33:56 +0100

    Added another method of reading DICOM files

---
+++ esther/test_read_2.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from pydicom import dcmread
+
+# Load the scans in given folder path
+def load_scan(path):
+    slices = [dcmread(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
+    return slices # list of DICOM
+
+path = "C:\\Users\\esthe\\OneDrive\\Desktop\\Mastergrad\\test1\\files2\\files"
+
+
+
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+        
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+        
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+            
+        image[slice_number] += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+patient = load_scan(path)
+patient_pixels = get_pixels_hu(patient)
+
+
+plt.hist(patient_pixels[150].flatten(), bins=80, color="c")
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
+
+plt.imshow(patient_pixels[150], cmap=plt.cm.gray)
+plt.show()

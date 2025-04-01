4f2af2c4a5d83d3d1f9a07006f652751e5faf41a
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2025-04-01 14:41:38 +0200

    Fixed write to folder in roi2nifti script

--- analysis/roi_to_nifti.py
+++ analysis/roi_to_nifti.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     folder_name = parser.parse_args().folder_name
 
     folder_path = f'../perf/{folder_name}'
-    nifti_path = f'../cropped_nifti_by_soft_tissue_window/'
+    nifti_path = f'../cropped_nifti_by_soft_tissue_window'
 
     with h5py.File(f'{folder_path}/predictions.h5', 'r') as f:
         for key in f['predicted'].keys():
@@ -20,5 +20,5 @@ if __name__ == '__main__':
             image = sitk.GetImageFromArray(f['predicted'][key][:, :original_x, :original_y])
             image.CopyInformation(sitk_image)
             output_roi_nifti_file = f'{nifti_path}/{key}_{folder_name.replace("_all", "")}_pred.nii.gz'
-            sitk.WriteImage(output_roi_nifti_file, output_roi_nifti_file)
+            sitk.WriteImage(image, output_roi_nifti_file)
             print(f'{key} done')
